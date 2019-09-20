1cf436e522223732cd1ad17117fff2591109b769
lianxianzhong <46148386+lianxianzhong@users.noreply.github.com>
2019-09-20 14:19:53 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -42,19 +42,19 @@ export default new Router({
       name: 'index',
       redirect: '/home',
       children: [
-        {path: 'home', component: Home},
-        {path: 'goods', component: GoodS},
-        {path: 'spread', component: Spread},
-        {path: 'jackpot', component: Jackpot},
-        {path: 'rakeBack', component: RakeBack},
-        {path: 'crowdFunding', component: CrowdFunding},
-        {path: 'orderList', name: 'orderList', component: OrderList},
-        {path: 'mineField', component: MineField},
-        {path: 'aboutUs', component: AboutUs},
-        {path: 'serviceAgreement', component: ServiceAgreement},
-        {path: 'goodsDetails', name: 'goodsDetails', component: goodsDetails},
-        {path: '/news', name: 'news', component: news},
-        {path: '/newDetails', name: 'newDetails', component: newDetails}
+        { path: 'home', component: Home },
+        { path: 'goods', component: GoodS },
+        { path: 'spread', component: Spread },
+        { path: 'jackpot', component: Jackpot },
+        { path: 'rakeBack', component: RakeBack },
+        { path: 'crowdFunding', component: CrowdFunding },
+        { path: 'orderList', name: 'orderList', component: OrderList },
+        { path: 'mineField', component: MineField },
+        { path: 'aboutUs', component: AboutUs },
+        { path: 'serviceAgreement', component: ServiceAgreement },
+        { path: 'goodsDetails', name: 'goodsDetails', component: goodsDetails },
+        { path: '/news', name: 'news', component: news },
+        { path: '/newDetails', name: 'newDetails', component: newDetails }
       ]
     },
     {
@@ -65,14 +65,14 @@ export default new Router({
         requireAuth: true
       }
     },
-    {path: '/cart', name: 'cart', component: Cart},
+    { path: '/cart', name: 'cart', component: Cart },
     {
       path: '/order',
       name: 'order',
       component: order,
       children: [
-        {path: 'paysuccess', name: 'paysuccess', component: paysuccess},
-        {path: 'payment', name: 'payment', component: payment}
+        { path: 'paysuccess', name: 'paysuccess', component: paysuccess },
+        { path: 'payment', name: 'payment', component: payment }
       ]
     },
     {
@@ -80,21 +80,21 @@ export default new Router({
       name: 'user',
       component: user,
       redirect: '/user/orderList',
-      meta: {requireAuth: true},
+      meta: { requireAuth: true },
       children: [
-        {path: 'orderList', name: '订单列表', component: orderList},
-        {path: 'myTeam', name: '我的团队', component: myTeam},
-        {path: 'information', name: '控制面板', component: information},
-        {path: 'addressList', name: '收益地址', component: addressList},
-        {path: 'coupon', name: '我的优惠', component: coupon},
-        {path: 'support', name: '售后服务', component: support},
-        {path: 'extension', name: '我的推广', component: extension},
-        {path: 'dailyOutput', name: '每日产出', component: dailyOutput},
-        {path: 'chargeRecharge', name: '电费充值', component: chargeRecharge},
-        {path: 'receivingAddress', name: '收货地址', component: receivingAddress}
+        { path: 'orderList', name: '订单列表', component: orderList },
+        { path: 'myTeam', name: '我的团队', component: myTeam },
+        { path: 'information', name: '控制面板', component: information },
+        { path: 'addressList', name: '收益地址', component: addressList },
+        { path: 'coupon', name: '我的优惠', component: coupon },
+        { path: 'support', name: '售后服务', component: support },
+        { path: 'extension', name: '我的推广', component: extension },
+        { path: 'dailyOutput', name: '每日产出', component: dailyOutput },
+        { path: 'chargeRecharge', name: '电费充值', component: chargeRecharge },
+        { path: 'receivingAddress', name: '收货地址', component: receivingAddress }
       ]
     },
-    {path: '/checkout', name: 'checkout', component: checkout},
-    {path: '*', redirect: '/home'}
+    { path: '/checkout', name: 'checkout', component: checkout },
+    { path: '*', redirect: '/home' }
   ]
 })
