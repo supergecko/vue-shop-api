1eacc2dcfd893354d552f9d85a8547bf78e4ae16
lianxianzhong <46148386+lianxianzhong@users.noreply.github.com>
2019-09-20 15:06:09 +0800

    Update notice.vue

--- src/common/notice.vue
+++ src/common/notice.vue
@@ -5,14 +5,12 @@
         <span>最新战报</span>
       </div>
       <div class="marquee_box">
-        <ul class="marquee_list" :class="{marquee_top:animate}">
-          <li v-for="(item, index) in marqueeList">
-            <span>{{item.name}}</span>
-            <span>在</span>
-            <span class="red"> {{item.city}}</span>
-            <span>杀敌</span>
-            <span class="red"> {{item.amount}}</span>
-            <span>万</span>
+        <ul class="marquee_list"
+            :class="{marquee_top:animate}">
+          <li v-for="(item, index) in marqueeList"
+              @click="toPath(index)">
+            <span>{{item.title}}</span>
+
           </li>
         </ul>
       </div>
@@ -21,112 +19,131 @@
 </template>
 
 <script>
-    export default {
-      data () {
-        return {
-          animate: false,
-          marqueeList: [
-            {
-              name: '1军',
-              city: '北京',
-              amount: '10'
-            },
-            {
-              name: '2军',
-              city: '上海',
-              amount: '20'
-            },
-            {
-              name: '3军',
-              city: '广州',
-              amount: '30'
-            },
-            {
-              name: '4军',
-              city: '重庆',
-              amount: '40'
-            }
-          ]
+import { marquee } from '/api/index'
+
+export default {
+  data () {
+    return {
+      animate: false,
+      marqueeList: [
+        {
+          name: '1军',
+          city: '北京',
+          amount: '10'
+        },
+        {
+          name: '2军',
+          city: '上海',
+          amount: '20'
+        },
+        {
+          name: '3军',
+          city: '广州',
+          amount: '30'
+        },
+        {
+          name: '4军',
+          city: '重庆',
+          amount: '40'
         }
-      },
-      created: function () {
-        setInterval(this.showMarquee, 2000)
-      },
-      methods: {
-        showMarquee: function () {
-          this.animate = true
-          setTimeout(() => {
-            this.marqueeList.push(this.marqueeList[0])
-            this.marqueeList.shift()
-            this.animate = false
-          }, 500)
-        }
-      }
+      ]
     }
+  },
+  created: function () {
+    setInterval(this.showMarquee, 2000)
+    this.getDate()
+  },
+  methods: {
+    showMarquee: function () {
+      this.animate = true
+      setTimeout(() => {
+        this.marqueeList.push(this.marqueeList[0])
+        this.marqueeList.shift()
+        this.animate = false
+      }, 500)
+    },
+    getDate () {
+      marquee()
+        .then(res => {
+          this.marqueeList = res.data.data
+        })
+    },
+    toPath (index) {
+      this.$router.push({
+        path: '/newDetails',
+        query: {
+          id: this.marqueeList[index].article_id
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
-  div, ul, li, span, img {
-    margin: 0;
-    padding: 0;
-    display: flex;
-    box-sizing: border-box;
-  }
-  .marquee {
-    width: 1220px;
-    align-items: center;
-    color: black;
-    background: #262a37;
-    display: flex;
-    box-sizing: border-box;
-  }
+div,
+ul,
+li,
+span,
+img {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  box-sizing: border-box;
+}
+.marquee {
+  width: 1220px;
+  align-items: center;
+  color: black;
+  background: #262a37;
+  display: flex;
+  box-sizing: border-box;
+}
 
-  .marquee_title {
-    height: 30px;
-    font-size: 14px;
-    border-right: 1px solid #d8d8d8;
-    align-items: center;
-    color: hsla(0,0%,100%,.6);
-    cursor: pointer;
-    padding-left: 0px;
-    padding-right: 20px;
-  }
+.marquee_title {
+  height: 30px;
+  font-size: 14px;
+  border-right: 1px solid #d8d8d8;
+  align-items: center;
+  color: hsla(0, 0%, 100%, 0.6);
+  cursor: pointer;
+  padding-left: 0px;
+  padding-right: 20px;
+}
 
-  .marquee_box {
-    display: block;
-    position: relative;
-    width: 60%;
-    height: 30px;
-    overflow: hidden;
-  }
+.marquee_box {
+  display: block;
+  position: relative;
+  width: 60%;
+  height: 30px;
+  overflow: hidden;
+}
 
-  .marquee_list {
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-  .marquee_top {
-    transition: all 0.5s;
-    margin-top: -30px
-  }
+.marquee_list {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.marquee_top {
+  transition: all 0.5s;
+  margin-top: -30px;
+}
 
-  .marquee_list li {
-    height: 30px;
-    line-height: 30px;
-    font-size: 14px;
-    padding-left: 20px;
-    color: hsla(0,0%,100%,.6);
-    cursor: pointer;
-  }
+.marquee_list li {
+  height: 30px;
+  line-height: 30px;
+  font-size: 14px;
+  padding-left: 20px;
+  color: hsla(0, 0%, 100%, 0.6);
+  cursor: pointer;
+}
 
-  .marquee_list li span {
-    padding: 0 2px;
-  }
+.marquee_list li span {
+  padding: 0 2px;
+}
 
-  .red {
-    color: #FF0101;
-  }
-
-
+.red {
+  color: #ff0101;
+}
 </style>
