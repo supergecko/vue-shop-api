a3c36d77365e8fbd5b69c97a65ef2d5c8609a63f
lianxianzhong <46148386+lianxianzhong@users.noreply.github.com>
2019-09-25 18:00:55 +0800

    Update usdtAccount.vue

--- src/common/usdtAccount.vue
+++ src/common/usdtAccount.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="account">
-    <button @click="showAdd"
-            class="btn">新增</button>
+  <div class="usdtUser">
     <el-table :data="tableData"
-              style="width: 100%;margin-bottom: 20px;margin-top:50px"
-              height="300">
-      <el-table-column prop="address"
-                       label="提币地址"
-                       width="300">
+              style="width: 100%;margin-bottom: 20px"
+              height="400">
+      <el-table-column prop="time"
+                       label="时间"
+                       width="160">
       </el-table-column>
-      <el-table-column prop="remark"
-                       label="地址备注"
-                       width="300">
+      <el-table-column prop="status"
+                       label="使用情况"
+                       width="200">
+      </el-table-column>
+      <el-table-column prop="money"
+                       label="金额"
+                       width="200">
+      </el-table-column>
+      <el-table-column prop="residue"
+                       label="剩余金额"
+                       width="200">
       </el-table-column>
       <el-table-column prop="tag"
-                       label="提现状态"
+                       label="状态"
                        width="200"
                        :filters="[
-      { text: '待提现', value: '待提现' },
+      { text: '待支付', value: '待支付' },
       { text: '确认中', value: '确认中' },
       { text: '已完成', value: '已完成' },
       { text: '已取消', value: '已取消' },
@@ -33,92 +39,54 @@
                   close-transition>{{scope.row.tag}}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button @click.native.prevent="deleteRow(scope.$index, tableData)"
-                     type="text"
-                     size="small">
-            撤回
-          </el-button>
-          <el-button type="text"
-                     size="small">提现</el-button>
-        </template>
-      </el-table-column>
     </el-table>
-    <div class="add"
-         v-show="isAdd">
-      <div class="title">
-        新增提现账号
-        <i @click="closeAdd"
-           class="el-icon-close"></i>
-      </div>
-      <div class="infoWrap">
-        <div class="inTitle">开卡银行<span style="color: red">*</span>:</div>
-        <input class="iput"
-               type="text"
-               v-model="addInfo.address">
-      </div>
-      <div class="infoWrap">
-        <div class="inTitle">开卡地区<span style="color: red">*</span>:</div>
-        <input class="iput"
-               type="text"
-               v-model="addInfo.remark">
-      </div>
-      <button style="margin-left: 130px"
-              class="addBtn"
-              @click="add">保存</button>
-      <button style="background: #ccc"
-              class="addBtn"
-              @click="cancel">取消</button>
-    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'account',
+  name: 'usdtUser',
   data: function () {
     return {
-      tableData: [
-        { address: '福建省福州市鼓楼区软件园', remark: '第三个地址', operation: '', tag: '已关闭' },
-        { address: '福建省福州市鼓楼区软件园', remark: '第二个地址', operation: '', tag: '已关闭' }
-      ],
-      isAdd: false,
-      addInfo: {
-        address: '',
-        remark: ''
+      tableData: [{
+        time: '2019/09/25 17:40',
+        status: '购买电费',
+        money: '1250',
+        residue: '200',
+        tag: '已完成'
+      },
+      {
+        time: '2019/09/25 17:40',
+        status: '购买电费',
+        money: '1250',
+        residue: '200',
+        tag: '已完成'
+      },
+      {
+        time: '2019/09/25 17:40',
+        status: '购买电费',
+        money: '1250',
+        residue: '200',
+        tag: '已完成'
+      },
+      {
+        time: '2019/09/25 17:40',
+        status: '购买电费',
+        money: '1250',
+        residue: '200',
+        tag: '已完成'
+      },
+      {
+        time: '2019/09/25 17:40',
+        status: '购买电费',
+        money: '1250',
+        residue: '200',
+        tag: '已完成'
       }
+      ]
     }
   },
   methods: {
-    showAdd () {
-      this.isAdd = true
-    },
-    closeAdd () {
-      this.isAdd = false
-    },
-    add () {
-      this.tableData.push(this.addInfo)
-      this.isAdd = false
-    },
-    cancel () {
-      this.isAdd = false
-    },
-    deleteRow (index, rows) {
-      this.$confirm('您确认撤回提现地址吗', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        rows.splice(index, 1)
-        this.$message({
-          type: 'success',
-          message: '撤回成功!'
-        })
-      }).catch(() => {
-
-      })
-    },
     formatter (row, column) {
       return row.orderID
     },
@@ -130,66 +98,4 @@ export default {
 </script>
 
 <style scoped>
-.account {
-  position: relative;
-}
-.btn {
-  background-color: rgb(244, 172, 40);
-  color: #fff;
-  padding: 10px 40px;
-  border-radius: 3px;
-  position: absolute;
-  top: -50px;
-  right: 0;
-}
-.add {
-  position: fixed;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  background-color: #fff;
-  width: 450px;
-  /* height: 300px; */
-  top: 380px;
-  left: 580px;
-  z-index: 999;
-}
-.title {
-  background-color: rgb(235, 235, 235);
-  padding: 5px 10px;
-  position: relative;
-  margin-bottom: 5px;
-}
-.el-icon-close {
-  position: absolute;
-  width: 30px;
-  height: 30px;
-  padding: 2px;
-  right: 0;
-  top: 0;
-  text-align: center;
-  line-height: 25px;
-}
-.infoWrap {
-  display: flex;
-  flex-direction: row;
-  padding: 5px 10px 0;
-  align-items: center;
-}
-.inTitle {
-  width: 90px;
-}
-.iput {
-  border: 1px solid #ccc;
-  padding: 10px;
-  width: 250px;
-}
-.addBtn {
-  background-color: rgb(244, 172, 40);
-  padding: 10px 30px;
-  border-radius: 3px;
-  color: #fff;
-  margin-top: 15px;
-  margin-left: 10px;
-  margin-bottom: 10px;
-}
 </style>
